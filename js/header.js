b2b4b07c5317595b7c1a7be638c458b60a5c1d01
d0rich <d.orich@yandex.ru>
2020-10-13 22:18:17 +0700

    header adapted

--- js/header.js
+++ js/header.js
@@ -5,4 +5,10 @@ $(document).ready(function () {
         $('html').animate({ scrollTop: destination }, 1100);
         return false;
     });
+    $("#nav-button").click(() => {
+        let buttons = $('#header-buttons')
+        if (!buttons.hasClass('active'))
+            buttons.addClass('active')
+        else buttons.removeClass('active')
+    })
 });
